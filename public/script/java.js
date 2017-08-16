62945b95f8823f1ad3533ec3c37a8576598f826d
Divyansh Kumar <kumar.divyansh1996@gmail.com>
2017-08-16 18:57:08 +0000

    yo

--- public/script/java.js
+++ public/script/java.js
@@ -91,7 +91,7 @@ $( window ).scroll(function() {
 
 
 	}
-	else if(document.body.scrollTop>=5446&&document.body.scrollTop<=6725){
+	else if(document.body.scrollTop>=5446&&document.body.scrollTop<=6325){
 	var styles = {
       background : "#ffd633"
       
