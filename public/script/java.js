c5f6e2ccd1962d4f6119d707e893b565a2ee9757
Divyansh Kumar <kumar.divyansh1996@gmail.com>
2017-08-18 15:13:36 +0000

    updated

--- public/script/java.js
+++ public/script/java.js
@@ -430,9 +430,23 @@ $(".chaar img").on("click", function(){
 
 
 
-$("#yelp a").on("click", function(){
- $('.close').click().animate({},672,'swing');
-})
+
+
+$('.pop-up').hide(0);
+$('.pop-up-container').hide(0);
+
+$('.pop-up-button').click(function(){
+  $('.pop-up-container').show(0);
+  $('.pop-up').fadeIn(300);
+  $('.pop-up-button').hide(0);
+});
+$('.pop-up span').click(function() {
+  $('.pop-up-container').hide(0);
+  $('.pop-up').hide(0);
+  $('.pop-up-button').show(0);
+});
+ 
+ 
  
  
  
